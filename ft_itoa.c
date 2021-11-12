e817433d22dee8d034f60f67e969ffb94f42d75b
Abdellah El bekkali <ael-bekk@e1r1p7.1337.ma>
2021-11-12 20:39:46 +0100

    libft

---
+++ ft_itoa.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-bekk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 14:07:57 by ael-bekk          #+#    #+#             */
+/*   Updated: 2021/11/09 14:17:03 by ael-bekk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_size_nb(int n)
+{
+	int	i;
+
+	i = 1 + !(n);
+	if (n < 0)
+		i++;
+	while (n)
+	{
+		i++;
+		n /= 10;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int n)
+{
+	char			*res;
+	int				i;
+	unsigned int	u;
+
+	i = ft_size_nb(n);
+	res = (char *)ft_calloc(i, 1);
+	if (!res)
+		return (NULL);
+	res[0] = '-';
+	res[--i] = '\0';
+	if (n < 0)
+		u = n * (-1);
+	else
+		u = n;
+	if (u == 0)
+		res[i - 1] = '0';
+	while (u)
+	{
+		res[--i] = (u % 10) + '0';
+		u /= 10;
+	}
+	return (res);
+}
---
+++ ft_strlcat.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-bekk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 13:51:44 by ael-bekk          #+#    #+#             */
+/*   Updated: 2021/11/09 14:15:24 by ael-bekk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dl;
+	size_t	sl;
+	size_t	i;
+
+	i = 0;
+	dl = ft_strlen(dst);
+	sl = ft_strlen(src);
+	if (!dst && !size)
+		return (sl);
+	if (dl < size)
+		sl += dl;
+	else
+		sl += size;
+	while (src[i] && (dl + i + 1) < size)
+	{
+		dst[dl + i] = ((char *)src)[i];
+		i++;
+	}
+	dst[dl + i] = '\0';
+	return (sl);
+}
