5d1217c43ffd6235e046a558882535c95cde1576
abdellah el bekkali <84334734+ael-bekk@users.noreply.github.com>
2021-11-13 18:31:38 +0100

    libft
    libft

--- ft_strdup.c
+++ ft_strdup.c
@@ -15,18 +15,16 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*s2;
-	int		i;
+	int		len;
 
-	i = 0;
-	while (s1[i])
-		i++;
-	s2 = (char *)malloc(i + 1);
+	len = ft_strlen(s1);
+	s2 = (char *)malloc(len + 1);
 	if (!s2)
 		return (NULL);
-	while (i >= 0)
+	while (len >= 0)
 	{
-		s2[i] = s1[i];
-		i--;
+		s2[len] = s1[len];
+		len--;
 	}
 	return (s2);
 }
